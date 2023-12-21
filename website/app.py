b6da42f158664bdbb44889da2a3617fe3f5ec61a
Whitespace404 <rush2asharahul@gmail.com>
2023-12-21 17:17:11 +0530

    database now works for apparatus insertion

--- website/app.py
+++ website/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sqlalchemy as sa
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
+app.secret_key = "sYqCrrQ3mUtsK9URfbKHG47Dvyer8TmKvtKA3_1Xqcg"
 db = SQLAlchemy(app)
 migrate = Migrate(app=app, db=db)
 
@@ -21,9 +24,23 @@ class Chemical(db.Model):
     count = sa.Column(sa.Integer)
 
 
-@app.route("/")
+class AddApparatusForm(FlaskForm):
+    name = StringField()
+    count = IntegerField()
+
+    submit = SubmitField("Add Apparatus")
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index2.html")
+    apparatuses = Apparatus.query.all()
+    form = AddApparatusForm()
+    if form.validate_on_submit():
+        apparatus = Apparatus(name=form.name.data, count=form.count.data)
+        db.session.add(apparatus)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("index2.html", apparatuses=apparatuses, form=form)
 
 
 if __name__ == "__main__":
