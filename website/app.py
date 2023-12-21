8426f20c6c7c253073af7f356c380afb5254e707
Whitespace404 <rush2asharahul@gmail.com>
2023-12-21 16:38:28 +0530

    fix bug/glitch where navbar preloads in a phantom state on initial paint

--- website/app.py
+++ website/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, flash
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index2.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
