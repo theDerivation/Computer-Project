64de47f185fe95f63c059e8446459dfe9bbc6fff
Whitespace404 <rush2asharahul@gmail.com>
2023-12-21 16:47:18 +0530

    create database file

--- website/app.py
+++ website/app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sqlalchemy as sa
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
+db = SQLAlchemy(app)
+migrate = Migrate(app=app, db=db)
+
+
+class Apparatus(db.Model):
+    sl_no = sa.Column(sa.Integer, primary_key=True, unique=True)
+    name = sa.Column(sa.String(60))
+    count = sa.Column(sa.Integer)
+
+
+class Chemical(db.Model):
+    sl_no = sa.Column(sa.Integer, primary_key=True, unique=True)
+    name = sa.Column(sa.String(60))
+    count = sa.Column(sa.Integer)
 
 
 @app.route("/")
